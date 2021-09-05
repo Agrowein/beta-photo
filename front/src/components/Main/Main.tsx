40e945bca6ea12335f1a12d1c9d7e3b13851eb2f
Agrowein <geo.harri40@gmail.com>
2021-09-05 22:38:06 +0300

    Created Drag and Drop area by 'react-file-drop' module

--- front/src/components/Main/Main.tsx
+++ front/src/components/Main/Main.tsx
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { DragEvent } from "react";
 
 import classes from './Main.module.scss'
+import { FileDrop } from 'react-file-drop'
 
 function Main() {
+    const dropFileEvent = (files: FileList | null, event: DragEvent) => {
+        console.log(files);
+    }
     return (
         <main className={classes.Main}>
-            main section 
+            <FileDrop 
+                className={classes.dropzone}
+                onDrop={dropFileEvent}
+            >DropZone</FileDrop>
         </main>
     );
 }
